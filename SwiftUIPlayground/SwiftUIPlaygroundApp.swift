0502d03a65b67a2711a210d5fb5749953f77ce55
dnlsmy <dnlsmy@gmail.com>
2022-01-24 11:43:09 -0500

    Initial Commit

---
+++ SwiftUIPlayground/SwiftUIPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPlaygroundApp.swift
+//  SwiftUIPlayground
+//
+//  Created by Danial Samy on 1/24/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
